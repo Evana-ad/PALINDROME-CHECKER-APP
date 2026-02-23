4daa395f0f50c8a2ccc941262835dbca53704568
Evana-ad <evana.carrier@gmail.com>
2026-02-23 09:27:41 +0530

    Base application setup

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,15 @@
+public class UseCase1PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("====================================");
+        System.out.println("      PALINDROME CHECKER APP");
+        System.out.println("====================================");
+        System.out.println("Version : 1.0.0");
+        System.out.println("Developer : PalindromeChecker Team");
+        System.out.println("====================================");
+        System.out.println("Application Started Successfully...");
+        System.out.println("====================================");
+
+    }
+}
