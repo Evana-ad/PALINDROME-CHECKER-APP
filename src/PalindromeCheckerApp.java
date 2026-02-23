b53a3f64068143a53f55770e5116b01c54565377
Evana-ad <evana.carrier@gmail.com>
2026-02-23 11:28:31 +0530

    UC6 implemented

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,29 +1,49 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
 import java.util.Scanner;
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("=== UC3: Palindrome Check Using String Reverse ===");
+            Scanner scanner = new Scanner(System.in);
+            Queue<Character> queue = new LinkedList<>();
+            Stack<Character> stack = new Stack<>();
 
-        System.out.print("Enter a string: ");
-        String original = scanner.nextLine();
+            System.out.println("====================================");
+            System.out.println("   PALINDROME CHECKER APP - UC6");
+            System.out.println("====================================");
 
-        String reversed = "";
+            System.out.print("Enter a string to check: ");
+            String input = scanner.nextLine();
 
-        for (int i = original.length() - 1; i >= 0; i--) {
-            reversed = reversed + original.charAt(i);
+
+            for (int i = 0; i < input.length(); i++) {
+                char ch = input.charAt(i);
+                queue.add(ch);   // FIFO
+                stack.push(ch);  // LIFO
+            }
+
+            boolean isPalindrome = true;
+
+
+            while (!queue.isEmpty() && !stack.isEmpty()) {
+                if (!queue.remove().equals(stack.pop())) {
+                    isPalindrome = false;
+                    break;
+                }
+            }
+
+
+            if (isPalindrome) {
+                System.out.println("\"" + input + "\" is a palindrome.");
+            } else {
+                System.out.println("\"" + input + "\" is NOT a palindrome.");
+            }
+
+            System.out.println("====================================");
+            scanner.close();
         }
-
-        if (original.equals(reversed)) {
-            System.out.println("Result: It is a Palindrome.");
-        } else {
-            System.out.println("Result: It is NOT a Palindrome.");
-        }
-
-        scanner.close();
-
 
     }
-}
