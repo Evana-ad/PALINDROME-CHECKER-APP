047688ec7a4c484517bbfc60357d93edd4d32d5e
Evana-ad <evana.carrier@gmail.com>
2026-03-02 08:57:28 +0530

    UC7: Add welcome message module

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,29 +1,49 @@
+
+import java.util.Deque;
+import java.util.ArrayDeque;
 import java.util.Scanner;
+
 public class PalindromeCheckerApp {
 
-    public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+        public static void main(String[] args) {
 
-        System.out.println("=== UC3: Palindrome Check Using String Reverse ===");
+            Scanner scanner = new Scanner(System.in);
+            Deque<Character> deque = new ArrayDeque<>();
 
-        System.out.print("Enter a string: ");
-        String original = scanner.nextLine();
+            System.out.println("====================================");
+            System.out.println("  PALINDROME CHECKER APP - UC7");
+            System.out.println("====================================");
 
-        String reversed = "";
+            System.out.print("Enter a string to check: ");
+            String input = scanner.nextLine();
 
-        for (int i = original.length() - 1; i >= 0; i--) {
-            reversed = reversed + original.charAt(i);
+
+            for (int i = 0; i < input.length(); i++) {
+                deque.addLast(input.charAt(i));
+            }
+
+            boolean isPalindrome = true;
+
+
+            while (deque.size() > 1) {
+                char front = deque.removeFirst();
+                char rear = deque.removeLast();
+
+                if (front != rear) {
+                    isPalindrome = false;
+                    break;
+                }
+            }
+
+
+            if (isPalindrome) {
+                System.out.println("\"" + input + "\" is a palindrome.");
+            } else {
+                System.out.println("\"" + input + "\" is NOT a palindrome.");
+            }
+
+            System.out.println("====================================");
+            scanner.close();
         }
-
-        if (original.equals(reversed)) {
-            System.out.println("Result: It is a Palindrome.");
-        } else {
-            System.out.println("Result: It is NOT a Palindrome.");
-        }
-
-        scanner.close();
-
-
     }
-}
