c146beb8fad9df9fe41a12fe661cadedcb579ca4
Evana-ad <evana.carrier@gmail.com>
2026-03-02 09:27:34 +0530

    UC8: Linked List Based Palindrome Checker

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,29 +1,77 @@
 import java.util.Scanner;
 public class PalindromeCheckerApp {
+    static class Node {
+        char data;
+        Node next;
+
+        Node(char data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
 
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("=== UC3: Palindrome Check Using String Reverse ===");
+        System.out.println("====================================");
+        System.out.println("  PALINDROME CHECKER APP - UC8");
+        System.out.println("====================================");
 
-        System.out.print("Enter a string: ");
-        String original = scanner.nextLine();
+        System.out.print("Enter a string to check: ");
+        String input = scanner.nextLine();
 
-        String reversed = "";
-
-        for (int i = original.length() - 1; i >= 0; i--) {
-            reversed = reversed + original.charAt(i);
+        if (input.length() == 0) {
+            System.out.println("Empty string is a palindrome.");
+            return;
         }
 
-        if (original.equals(reversed)) {
-            System.out.println("Result: It is a Palindrome.");
+
+        Node head = new Node(input.charAt(0));
+        Node current = head;
+
+        for (int i = 1; i < input.length(); i++) {
+            current.next = new Node(input.charAt(i));
+            current = current.next;
+        }
+
+
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        Node prev = null;
+        Node nextNode;
+
+        while (slow != null) {
+            nextNode = slow.next;
+            slow.next = prev;
+            prev = slow;
+            slow = nextNode;
+        }
+        Node firstHalf = head;
+        Node secondHalf = prev;
+        boolean isPalindrome = true;
+
+        while (secondHalf != null) {
+            if (firstHalf.data != secondHalf.data) {
+                isPalindrome = false;
+                break;
+            }
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
+        }
+        if (isPalindrome) {
+            System.out.println("\"" + input + "\" is a palindrome.");
         } else {
-            System.out.println("Result: It is NOT a Palindrome.");
+            System.out.println("\"" + input + "\" is NOT a palindrome.");
         }
 
+        System.out.println("====================================");
         scanner.close();
-
-
     }
 }
