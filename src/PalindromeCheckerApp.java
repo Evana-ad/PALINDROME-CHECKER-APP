09e78f7e52f95efca4e3654b44546455348acb5f
Evana-ad <evana.carrier@gmail.com>
2026-03-02 10:13:58 +0530

    UC12: Strategy Pattern Implementation

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,29 +1,78 @@
-import java.util.Scanner;
-public class PalindromeCheckerApp {
+import java.util.*;
 
-    public static void main(String[] args) {
+interface PalindromeStrategy {
+    boolean check(String input);
+}
 
-        Scanner scanner = new Scanner(System.in);
+class StackStrategy implements PalindromeStrategy {
 
-        System.out.println("=== UC3: Palindrome Check Using String Reverse ===");
+    public boolean check(String input) {
+        Stack<Character> stack = new Stack<>();
 
-        System.out.print("Enter a string: ");
-        String original = scanner.nextLine();
+        for (char c : input.toCharArray())
+            stack.push(c);
 
-        String reversed = "";
+        for (char c : input.toCharArray())
+            if (c != stack.pop())
+                return false;
 
-        for (int i = original.length() - 1; i >= 0; i--) {
-            reversed = reversed + original.charAt(i);
-        }
-
-        if (original.equals(reversed)) {
-            System.out.println("Result: It is a Palindrome.");
-        } else {
-            System.out.println("Result: It is NOT a Palindrome.");
-        }
-
-        scanner.close();
-
-
+        return true;
     }
+}
+
+class DequeStrategy implements PalindromeStrategy {
+
+    public boolean check(String input) {
+        Deque<Character> deque = new ArrayDeque<>();
+
+        for (char c : input.toCharArray())
+            deque.addLast(c);
+
+        while (deque.size() > 1)
+            if (deque.removeFirst() != deque.removeLast())
+                return false;
+
+        return true;
+    }
+}
+
+public class PalindromeCheckerApp{
+public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("====================================");
+    System.out.println("  PALINDROME CHECKER APP - UC12");
+    System.out.println("====================================");
+
+    System.out.println("Choose Strategy:");
+    System.out.println("1. Stack Strategy");
+    System.out.println("2. Deque Strategy");
+    System.out.print("Enter choice: ");
+
+    int choice = scanner.nextInt();
+    scanner.nextLine();
+
+    System.out.print("Enter a string to check: ");
+    String input = scanner.nextLine();
+
+    PalindromeStrategy strategy;
+
+    if (choice == 1)
+        strategy = new StackStrategy();
+    else
+        strategy = new DequeStrategy();
+
+    boolean result = strategy.check(input);
+
+    if (result)
+        System.out.println("\"" + input + "\" is a palindrome.");
+    else
+        System.out.println("\"" + input + "\" is NOT a palindrome.");
+
+    System.out.println("====================================");
+
+    scanner.close();
+}
+
 }
